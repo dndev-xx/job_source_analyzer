8c7a11495b38555d267e017685ac6023d6e3ae08
dndev-xx <dndev@mail.ru>
2024-02-04 01:04:21 +0300

    JSA-040224 {add specification,checkstyle}

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,32 @@
 plugins {
     kotlin("jvm") version "1.9.22"
+    id("checkstyle")
 }
 
-group = "ru.jsa"
-version = "1.0.0"
-
-repositories {
-    mavenCentral()
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.8.10")
 }
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven {
+            url = uri("https://jitpack.io")
+        }
+    }
+}
+subprojects {
+    group = "ru.jsa"
+    version = "1.0.0"
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions.jvmTarget = "17"
+    }
+    tasks.withType<Checkstyle> {
+        configFile = project.file("configuration/checkstyle/checkstyle.xml")
+    }
+}
+
+
+
+
